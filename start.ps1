c1d4be9c5dde27ca189c572f170be654a04b06ff
ste <ste@mbp16>
2026-03-30 11:43:48 +0200

    Refactor start.ps1 to ensure virtual environment is created if it doesn't exist

--- start.ps1
+++ start.ps1
@@ -1,5 +1,10 @@
 # If execution is blocked, run once: Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
 Set-Location $PSScriptRoot
+if (-not (Test-Path .venv)) {
+    Write-Host "Creating virtual environment..."
+    python -m venv .venv
+    & .venv\Scripts\pip install -r requirements.txt
+}
 & .venv\Scripts\Activate.ps1
 $env:PATH = "$PSScriptRoot\leapc_cffi;$env:PATH"
 python main.py @args
